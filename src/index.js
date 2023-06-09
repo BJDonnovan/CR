96f5b04fcd6e240c343bc98a31bef9e00b760dff
BJDonnovan <mihailshulak079@gmail.com>
2023-06-09 22:04:39 +0700

    try to nw

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { Products } from "./scripts/Products";
 import { CATALOG } from "./scripts/catalog";
-import { SetClick } from "./scripts/main";
-import './styles/Products.css';
-var productsPage = new Products();
+// import { SetClick } from "./scripts/Products";
+import './styles/styles.css';
+const productsPage = new Products();
 productsPage.render();
 for(let i = 0;i<CATALOG.length;i++)
 {
     var element = document.getElementById(CATALOG[i].id);
-    element.addEventListener('click',SetClick(element,CATALOG[i].id));
+    element.onclick = productsPage.SetClick(element,CATALOG[i].id)
 }
 
 
--- src/scripts/Products.js
+++ src/scripts/Products.js
@@ -3,10 +3,21 @@ import { localStorageUtil } from "./localStorageUtils";
 
 export class Products {
     constructor() {
-        
         this.classNameActive = 'products-element__btn_active';
         this.labelAdd = 'Добавить в корзину';
         this.labelRemove = 'Удалить из корзины';
+    }
+
+    SetClick(element,id) {
+
+        const { pushProduct, products } = localStorageUtil.putProducts(id);
+        if (pushProduct) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML = this.labelRemove;
+        } else {
+            element.classList.remove(this.classNameActive);
+            element.innerHTML = this.labelAdd;
+        }
     }
 
     render() {
@@ -14,14 +25,13 @@ export class Products {
         const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
 
-        CATALOG.forEach(({ id, name, price, img }) => {
+        CATALOG.forEach(({ id, name, price, img}) => {
             let activeClass = '';
             let activeText = '';
 
             if (productsStore.indexOf(id) === -1) {
                 activeText = this.labelAdd;
             } else {
-                activeClass = this.classNameActive;
                 activeText = this.labelRemove;
             }
 
@@ -32,7 +42,7 @@ export class Products {
                     <span class="products-element__price">
                          ${price.toLocaleString()} Руб
                     </span>
-                    <button id ="${id}" class="products-element__btn ${activeClass}">
+                    <button id ="${id}" class="products-element__btn">
                         ${activeText}
                     </button>
                 </li>
@@ -47,20 +57,8 @@ export class Products {
         `;
 
         PRODUCTS.innerHTML = html;
+       
     }
 
-    
 }
 
-export function handleSetLocationStorage() {
-    console.log('1');
-    // const { pushProduct, products } = this.localStorageUtil.putProducts(id);
-    
-    // if (pushProduct) {
-    //     element.classList.add(this.classNameActive);
-    //     element.innerHTML = this.labelRemove;
-    // } else {
-    //     element.classList.remove(this.classNameActive);
-    //     element.innerHTML = this.labelAdd;
-    // }
-}
